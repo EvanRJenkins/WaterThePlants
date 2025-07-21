aabb75a779cf71f9fbaaf05cc2e9667be6bba658
Evan Jenkins <JenkinsE@jbu.edu>
2025-07-21 14:56:22 -0500

    Update ArduinoMain.cpp
    Revised state transition pattern by removing the redundant INTERRUPT state. Added missing break statements to the main loop switch statement. Broke down sysInit() tasks into smaller functions that initialize pin assignments and Plant characteristics.

--- ArduinoMain.cpp
+++ ArduinoMain.cpp
@@ -5,11 +5,9 @@
 #define AI0 PC0
 #define PUMP_PIN PD2  // To-do: Allocate pins to peripherals
 
-SMCR |= (1 << SM1);  // Set Sleep Mode Control Register to Power Down Mode
 
 typedef enum State {  // New type for state machine
   IDLE,
-  INTERRUPT,
   WATERING,
   LOGGING,
   ERROR
@@ -17,51 +15,72 @@ typedef enum State {  // New type for state machine
 
 
 typedef struct {                         // Type to hold key characteristics of unique plants
-  unsigned char species[10];
-  unsigned char sensorPin;
-  unsigned int moistureLevel;
+  unsigned char species[10];             // String holding plant species name
+  unsigned char sensorPin;               // Analog Input Pin for Plant's moisture sensor
+  unsigned int moistureLevel;            // Most recently logged moisture level
   unsigned char targetMoistureRange[2];  // Indices define bounds of % range for ideal soil saturation
 } Plant;
 
 
-Plant hibiscus;                        // Instantiation of Plant hibiscus
-strcpy(hibiscus.species, "hibiscus");  // Plant species assignment
-hibiscus.sensorPin = AI0;              // Moisture sensor input pin
-hibiscus.targetMoistureRange[0] = 50;  // Lower Bound
-hibiscus.targetMoistureRange[1] = 75;  // Upper Bound
+Plant hibiscus;  // Instantiation of Plant hibiscus
 
 state_t currentState;  // Holds current system state
 
 state_t sysInit();
 
-state_t sleep();
-
-state_t wakeUp();
+void definePlants();
 
 int main(void) {
+  
   currentState = sysInit();  // Initialize system
 
   while (1) {  // State machine loop
+    
     switch (currentState) {
       case IDLE:
-        // SREG |= (1 << I);    // Enable global interrupts
+        // sei();               // Enable global interrupts
         // sleep();             // Call AVR Sleep instruction
-      case: INTERRUPT:
-        // Decide what to do based on interrupt that occured
+        break;
       case WATERING:
         // call start watering function
         // update state
+        break;
       case LOGGING:
         // log desired data
         // update state
+        break;
       case ERROR:
         // perform any fail safety actions
         // wait for error to clear
-        // update state
-        
+        // update state  
+        break;
     }
-  
   }
-  
+}
 
+
+void definePlants() {                    // Defines attributes of existing Plants
+
+  // Definition of Plant hibiscus
+  strcpy(hibiscus.species, "hibiscus");  // Plant species assignment
+  hibiscus.sensorPin = AI0;              // Moisture sensor input pin
+  hibiscus.targetMoistureRange[0] = 50;  // Lower Bound
+  hibiscus.targetMoistureRange[1] = 75;  // Upper Bound
+
+  // Add any additional plants here
+}
+
+
+state_t sysInit() {
+  
+  SMCR |= (1 << SM1);       // Set Sleep Mode Control Register to Power Down Mode
+
+  DDRD |= (1 << PUMP_PIN);  //Set pump pin data direction to output
+
+  //Insert all pin configurations
+
+  definePlants();
+
+  //if something goes wrong, return ERROR;
+  //else return LOGGING; // End initialization by entering LOGGING state to record startup values
 }
