fe0715766ea8752d7ce3e301999650c21bee983d
Evan Jenkins <JenkinsE@jbu.edu>
2025-07-21 12:48:26 -0500

    Update ArduinoMain.cpp
    Added statements to initialize sleep mode register to "power down" mode and added an interrupt state.

--- ArduinoMain.cpp
+++ ArduinoMain.cpp
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <avr/io.h>
 
+#define PUMP_PIN ...  // To-do: Allocate pins to peripherals
+#define ...
+
+SMCR |= (1 << SM1);  // Set Sleep Mode Control Register to Power Down Mode
 
 typedef enum sysState {  // New type for state machine
   IDLE,
+  INTERRUPT,
   WATERING,
   LOGGING,
   ERROR
@@ -14,7 +19,9 @@ state_t currentState;  // Holds current system state
 
 state_t sysInit();
 
+state_t sleep();
 
+state_t wakeUp();
 
 int main(void) {
   currentState = sysInit();  // Initialize system
@@ -22,7 +29,10 @@ int main(void) {
   while (1) {  // State machine loop
     switch (currentState) {
       case IDLE:
-        // wait
+        // SREG |= (1 << I);    // Enable global interrupts
+        // sleep();             // Call AVR Sleep instruction
+      case: INTERRUPT:
+        // Decide what to do based on interrupt that occured
       case WATERING:
         // call start watering function
         // update state
