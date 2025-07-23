57e66050d90a21cb91cdc9430ccf9ae9bfb6035e
Evan Jenkins <JenkinsE@jbu.edu>
2025-07-23 14:14:38 -0500

    Update interrupts.h

--- include/interrupts.h
+++ include/interrupts.h
@@ -3,6 +3,7 @@
 
 #include "main.h"
 
+
 /* Interrupt bitfield bits */
 
 #define PCINT0 (1 << 0)                   // Bit 0 is PCINT0
@@ -11,8 +12,7 @@
 #define NEW_PULSE_DATA (1 << 3)           // Bit 3 is new pulse data ready
 
 
-/* External declarations for global variables */
-
+/* Extern declarations for global variables */
 
 extern volatile uint8_t g_isrFlags;
 extern volatile uint32_t g_msCounter;
