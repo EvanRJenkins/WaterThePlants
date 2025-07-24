98a25b2d450a9469f417692a07fe1d5ce16e552f
Evan Jenkins <JenkinsE@jbu.edu>
2025-07-24 13:26:11 -0500

    Update hal.h

--- program_files/include/hal.h
+++ program_files/include/hal.h
@@ -4,9 +4,7 @@
 #include "main.h"
 #include <avr/io.h>
 
-
 /* Pin aliases (Arduino Uno R3) */
-
 #define AI0 PC0
 #define AI1 PC1
 #define AI2 PC2
@@ -16,13 +14,10 @@
 #define FLOW_SENSOR_PIN PD2
 #define PUMP_PIN PD3
 
-
 /* Function Prototypes */
-
 void regConfig();
 state_t adcRecord(uint8_t targetPin);
 state_t startPump();
 state_t stopPump();
-
 
 #endif // HAL_H
