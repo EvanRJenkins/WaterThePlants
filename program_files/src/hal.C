2f198a0cbaa6b579879aa5e7bd440be468b63ac6
Evan Jenkins <JenkinsE@jbu.edu>
2025-07-24 13:06:18 -0500

    Update hal.C
    fixed whitespace

--- program_files/src/hal.C
+++ program_files/src/hal.C
@@ -1,7 +1,8 @@
 #include "hal.h"
 #include <avr/interrupt.h>
 
-void regConfig() {
+void regConfig() 
+{
   DDRB &= ~(1 << DDB0);                         // Set trigger pin direction as input (D8)
   DDRD &= ~(1 << FLOW_SENSOR_PIN);              // Set flow sensor pin data direction to input (D2)
   PORTB |= (1 << PORTB0);                       // Enable pull-up on pin D8
@@ -9,7 +10,7 @@ void regConfig() {
   DDRD |= (1 << PUMP_PIN);                      // Set pump pin data direction to output
 
   /* Sleep mode */
-  // SMCR |= (1 << SM1);                        // Set Sleep Mode Control Register to Power Down Mode
+  SMCR |= (1 << SM1);                           // Set Sleep Mode Control Register to Power Down Mode
   
   /* Enable interrupts */
   ADCSRA |= (1 << ADIE);                        // Enable 'ADC complete' interrupt  
@@ -19,7 +20,7 @@ void regConfig() {
 
   /* Initialize Timer/Counter0 (watering) */
   TCCR0B &= ~((1 << CS02) | (1 << CS01) | (1 << CS00)); // Select no clock for Timer/Counter0
-  TCNT0 = 0;                                    // Clear Timer/Counter0 data register
+  TCNT0 = 0;                                            // Clear Timer/Counter0 data register
 
   /* Configure External Interrupt INT0 for Flow Sensor */
   EICRA |= (1 << ISC01);                        // Set INT0 to trigger on a falling edge
@@ -32,59 +33,57 @@ void regConfig() {
   OCR1A = 249;                                  // Set the compare match register for 1ms tick
 }
 
-state_t adcRecord(uint8_t targetPin) {    // Enables ADC and starts conversion for targetPin
+state_t adcRecord(uint8_t targetPin)  // Enables ADC and starts conversion for targetPin
+{
 /* Set MUX2..0 to ADC channel of targetPin */
- switch (targetPin) {
+  switch (targetPin) 
+  {
+    case AI0:  // 000
+      ADMUX &= ~((1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+      break;
   
-   case AI0:  // 000
-    ADMUX &= ~((1 << MUX2) | (1 << MUX1) | (1 << MUX0));
-    break;
+    case AI1:  // 001
+      ADMUX &= ~((1 << MUX2) | (1 << MUX1));
+      ADMUX |= (1 << MUX0);
+      break;
   
-   case AI1:  // 001
-    ADMUX &= ~((1 << MUX2) | (1 << MUX1));
-    ADMUX |= (1 << MUX0);
-    break;
+    case AI2:  // 010
+      ADMUX &= ~((1 << MUX2) | (1 << MUX0));
+      ADMUX |= (1 << MUX1);
+      break;
   
-   case AI2:  // 010
-    ADMUX &= ~((1 << MUX2) | (1 << MUX0));
-    ADMUX |= (1 << MUX1);
-    break;
+    case AI3:  // 011
+      ADMUX &= ~(1 << MUX2);
+      ADMUX |= (1 << MUX1) | (1 << MUX0);
+      break;
   
-   case AI3:  // 011
-    ADMUX &= ~(1 << MUX2);
-    ADMUX |= (1 << MUX1) | (1 << MUX0);
-    break;
+    case AI4:  // 100
+      ADMUX &= ~((1 << MUX1) | (1 << MUX0));
+      ADMUX |= (1 << MUX2);
+      break;
   
-   case AI4:  // 100
-    ADMUX &= ~((1 << MUX1) | (1 << MUX0));
-    ADMUX |= (1 << MUX2);
-    break;
-  
-   case AI5:  // 101
-    ADMUX &= ~(1 << MUX1);
-    ADMUX |= (1 << MUX2) | (1 << MUX0);
-    break;
-  
-   default: // Default to channel 0
-    ADMUX &= ~((1 << MUX2) | (1 << MUX1) | (1 << MUX0));
-    break;
- }
-
+    case AI5:  // 101
+      ADMUX &= ~(1 << MUX1);
+      ADMUX |= (1 << MUX2) | (1 << MUX0);
+      break;
+    
+    default: // Default to channel 0
+      ADMUX &= ~((1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+      break;
+  }
   ADCSRA |= (1 << ADEN);  // Enable ADC
   ADCSRA |= (1 << ADSC);  // Start ADC conversion
-
   return WAIT_FOR_ADC;
 }
 
-
-state_t startPump() {
+state_t startPump() 
+{
   PORTD |= (1 << PUMP_PIN);   // Activate tank pump  
-  
   return WAIT_FOR_FLOW;
 }
 
-
-state_t stopPump() {
+state_t stopPump() 
+{
   PORTD &= ~(1 << PUMP_PIN);  // Deactivate tank pump  
   
   return LOG_POST;
